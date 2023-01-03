d4f7ff82904e43e4645ef5c40b5197769502ba80
TICK-ING <119712541+TICK-ING@users.noreply.github.com>
2023-01-03 19:16:33 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '服务器状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm793349267-d72108a10d5e88574dff7b90',
+    'm793389809-ac76e9fa8f8915f9ef22330e',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+  
+  ],
+};
